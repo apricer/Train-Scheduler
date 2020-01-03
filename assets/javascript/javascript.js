6d4d3244f350fb3bc56bdecf878a30cfaaada907
Anthony Pricer <anthony.pricer@outlook.com>
2020-01-02 21:38:41 -0600

    Train-Scheduler

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAyVQ3eg1ez08-KT85EfETVaN25csWyHiA",
+    authDomain: "train-scheduler-1934f.firebaseapp.com",
+    databaseURL: "https://train-scheduler-1934f.firebaseio.com",
+    projectId: "train-scheduler-1934f",
+    storageBucket: "train-scheduler-1934f.appspot.com",
+    messagingSenderId: "212664747432",
+    appId: "1:212664747432:web:1fe3a1533f6d30547c099d",
+    measurementId: "G-2E1LRRDL6W"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
