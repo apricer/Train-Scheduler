fd1d2d3c50b55005c41df6a07054c5a8f0dc539a
Anthony Pricer <anthony.pricer@outlook.com>
2020-01-25 17:01:28 -0600

    Train-Scheduler

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,5 @@
-<script>
-    var firebaseConfig = {
-        apiKey: "AIzaSyAyVQ3eg1ez08-KT85EfETVaN25csWyHiA",
+var config = {
+    apiKey: "AIzaSyAyVQ3eg1ez08-KT85EfETVaN25csWyHiA",
     authDomain: "train-scheduler-1934f.firebaseapp.com",
     databaseURL: "https://train-scheduler-1934f.firebaseio.com",
     projectId: "train-scheduler-1934f",
@@ -9,14 +8,12 @@
     appId: "1:212664747432:web:1fe3a1533f6d30547c099d",
     measurementId: "G-2E1LRRDL6W"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-</script>
+
+firebase.initializeApp(config);
 
 var trainData = firebase.database();
 
-$(#addTrainBtn).on("click", function () {
+$("#addTrainBtn").on("click", function () {
     var trainName = $("#trainNameInput").val().trim();
     var destination = $("#destinationInput").val().trim();
     var firstTrain = moment($("#firstTrainInput").val().trim(), "HH;mm").subtract(10, "years").format("X");
