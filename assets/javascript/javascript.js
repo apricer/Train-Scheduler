12945aaec101c99981080571aa42093b4cd45654
Anthony Pricer <anthony.pricer@outlook.com>
2020-01-26 00:03:38 -0600

    Train-Scheduler

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -19,6 +19,36 @@ $("#addTrainBtn").on("click", function () {
     var firstTrain = moment($("#firstTrainInput").val().trim(), "HH;mm").subtract(10, "years").format("X");
     var frequency = $("#frequencyInput").val().trim();
 
-    console.log(firstTrain);
+    var newTrain = {
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+    }
+
+    trainData.ref().push(newTrain);
+
+    alert("Train Added!");
+
+    $("trainNameInput").val("");
+    $("destinationInput").val("");
+    $("firstTrainInput").val("");
+    $("frequencyInput").val("");
+
     return false;
+})
+
+trainData.ref().on("child_added", function (snapshot) {
+    var name = snapshot.val().name;
+    var destination = snapshot.val().destination;
+    var frequency = snapshot.val().frequency;
+    var firstTrain = snapshot.val().firstTrain;
+
+    var remainder = moment().diff(moment.unix(firstTrain), "minutes") % frequency;
+    var minutes = frequency - remainder;
+    var arrival = moment().add(minutes, "m").format("hh:mm A");
+
+    console.log(remainder);
+    console.log(minutes);
+    console.log(arrival);
 })
